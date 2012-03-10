a24ba5e493c137284c5840ff5ea90ca88d550eab
Patrick Baselier <patrick@kabisa.nl>
2012-03-10 12:54:32 +0100

    Added helper for twitter link and styled footer.

--- views/view_helpers.rb
+++ views/view_helpers.rb
@@ -4,7 +4,7 @@
 # views pristine.
 #
 module ViewHelpers
-  
+
   # Calculate the years for a copyright
   def copyright_years(start_year)
     end_year = Date.today.year
@@ -14,12 +14,27 @@ module ViewHelpers
       "\#{start_year}&#8211;\#{end_year}"
     end
   end
-  
+
   # Handy for hiding a block of unfinished code
   def hidden(&block)
     #no-op
   end
-  
+
   # Add your own helpers below...
-  
+  def twitter_follow_button(user, options = {})
+    default_options = {
+      'class'           => 'twitter-follow-button',
+      'data-align'      => 'right',
+      'data-lang'       => 'en',
+      'data-show-count' => true
+    }
+    # Remove @ if present.
+    user.gsub!(/^@/,'')
+    content = link_to("Follow @#{user}", "https://twitter.com/#{user}", default_options.merge(options))
+    content << '<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="//platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>'
+
+    content
+
+  end
+
 end
