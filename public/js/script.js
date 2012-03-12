0440446ef42b4e863bb0e84a6784b6d1202bae48
Patrick Baselier <patrick@kabisa.nl>
2012-03-12 08:30:03 +0100

    Progress bar.

--- public/js/script.js
+++ public/js/script.js
@@ -1,7 +1,43 @@
-/* Author: 
+/* Author:
 
 */
+var TIMOCO = {
+  PROGRESS: {
+    meter: function() {
+      return $('.meter');
+    }
+  , caption: function() {
+      return this.meter().children('p').children('span');
+    }
+  , draw: function() {
+      this.meter().children('p').before('<span></span>');
+      var completed = this.caption().html();
+      this.update(completed);
+    }
+  , update: function(width) {
+      this.meter().children('span').animate(
+        {
+          width: width
+        }
+      , {
+        duration: 3000
+      , easing: 'swing'
+      , step: function (now, fx) {
+        var progress = parseInt(now) + "%";
+        TIMOCO.PROGRESS.caption().html(progress);
+       }
+      });
+    }
+  }
 
+, init: function() {
+    this.PROGRESS.draw();
+  }
+};
+
+$(function() {
+  TIMOCO.init();
+});
 
 
 
